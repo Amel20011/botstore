ac2480f1241053e4b94561f8933390e7b2ced5e5
Amel20011 <amelcns8@gmail.com>
2025-12-12 13:42:13 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    sessionName: 'session',
+    prefix: '#',
+    ownerNumber: ['628xxxxxxxxxx'],
+    adminNumber: ['628xxxxxxxxxx'],
+    botNumber: '628xxxxxxxxxx',
+    databasePath: './database.json',
+    timezone: 'Asia/Jakarta',
+    footer: 'Bot Store © 2024',
+    welcomeVideo: './media/welcome.mp4',
+    storeBanner: './media/banner_store.jpg',
+    thumb: './media/thumb.jpg',
+    
+    // Daftar produk
+    products: [
+        { id: 1, name: 'ALIGHT MOTION PREMIUM', stock: 15, price: 15000 },
+        { id: 2, name: 'CANVA LIFETIME', stock: 7, price: 20000 },
+        { id: 3, name: 'CANVA PRO', stock: 36, price: 10000 },
+        { id: 4, name: 'CAPCUT PRO', stock: 180, price: 5000 },
+        { id: 5, name: 'CAPCUT PRO HEAD', stock: 17, price: 10000 },
+        { id: 6, name: 'CHATGPT PLUS', stock: 19, price: 25000 },
+        { id: 7, name: 'PICSART PRO', stock: 3, price: 15000 },
+        { id: 8, name: 'PRIME VIDEO', stock: 2, price: 20000 },
+        { id: 9, name: 'SCRIBD PREMIUM', stock: 3, price: 10000 },
+        { id: 10, name: 'SPOTIFY PREMIUM', stock: 38, price: 15000 }
+    ]
+};
